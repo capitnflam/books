7c916666a79845ee4e90d54ad998840a989d610d
capitN.flam <capitnflam@gmail.com>
2022-10-16 05:27:43 +0200

    feat(front): add mui

--- books-front/src/index.tsx
+++ books-front/src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseLine from '@mui/material/CssBaseline'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 const App = () => <div>Hello World!</div>
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseLine />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
