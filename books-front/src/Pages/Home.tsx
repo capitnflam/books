1bb5509d1f06a9f0ac039c105be92b8a2b62395b
capitN.flam <capitnflam@gmail.com>
2022-10-22 01:33:28 +0200

    feat: add home

---
+++ books-front/src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Center, Title } from '@mantine/core'
+
+export const Home = () => {
+  return (
+    <>
+      <Center>
+        <Title>Home</Title>
+      </Center>
+      sdmlfkjslkqmfjkqmlklskdjflskjdmqjmsklsjdfmqjqmskjdmlfjqjmlsjdmflkjsmdjmqljdfqmjsdmkjslmdjflksjflkjqfmldjhmlksqjfmlskqjfmlkqsjfmqsdlkjfmlqskdjfmlkqsjdlkfjsqmlkfjdsqmlfjqsmldkfjmqlkjdmlsjqmlkdjqmlskjdfmlqskjdfmlskdjfmlskdjfmqsldkjfmlsqdkjflmsdkjqmslkjdfmlskdjfslmkdjfsmldkjfmlskqdjfmqsdlkjfmsqldkjf
+    </>
+  )
+}
