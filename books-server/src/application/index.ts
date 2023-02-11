ef52f5e40d89eea1b5d7bb9e1617c035fcff242d
capitN.flam <capitnflam@gmail.com>
2023-02-11 21:23:04 +0100

    chore: remove useless files

--- books-server/src/application/index.ts
+++
@@ -1,2 +0,0 @@
-export { CONFIG_TOKEN, ApplicationConfiguration } from './config'
-export { ApplicationModule } from './module'
--- books-server/src/logger/index.ts
+++
@@ -1,3 +0,0 @@
-export { PinoLogger as Logger } from 'nestjs-pino'
-export { LoggerConfiguration } from './config'
-export { LoggerModule } from './module'
