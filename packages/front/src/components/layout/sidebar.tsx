58f84140d86850b2afe80e8d5692330a70faf986
capitN.flam <capitnflam@gmail.com>
2023-10-08 05:33:09 +0200

    wip

--- packages/front/src/components/layout/sidebar.tsx
+++ packages/front/src/components/layout/sidebar.tsx
@@ -1,22 +1,24 @@
 import { BookOpenIcon, PencilIcon } from '@heroicons/react/24/solid'
-import { useLocation, useMatches } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 
 import { sidebarOpenState } from './state'
 
 export function Sidebar() {
   const location = useLocation()
-  const matches = useMatches()
+  // const matches = useMatches()
   const isOpen = useRecoilValue(sidebarOpenState)
-  const width = isOpen ? 'w-80' : 'w-8'
+  const width = isOpen ? 'w-auto' : 'w-8 overflow-hidden'
 
   return (
-    <aside className="bg-blue-900 text-slate-100">
+    <aside className="bg-blue-900 p-2 text-slate-100">
       <nav className={width}>
-        <BookOpenIcon className="h-6 w-6" />
-        <pre>{JSON.stringify(location, null, 2)}</pre>
-        <PencilIcon className="h-6 w-6" />
-        <pre>{JSON.stringify(matches, null, 2)}</pre>
+        <div className="flex flex-col items-baseline">
+          <BookOpenIcon className="h-6 w-6" />
+          <pre>{JSON.stringify(location, null, 2)}</pre>
+          <PencilIcon className="h-6 w-6" />
+          {/* <pre>{JSON.stringify(matches, null, 2)}</pre> */}
+        </div>
       </nav>
     </aside>
   )
