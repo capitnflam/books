5e8ef364936dde0e8418356e8a75a30f78251008
capitN.flam <capitnflam@gmail.com>
2023-10-20 19:58:59 +0200

    updates

--- packages/front/prettier.config.js
+++ packages/front/prettier.config.js
@@ -1,11 +1,9 @@
-const tailwind = require('prettier-plugin-tailwindcss')
-
 const baseConfig = require('../../prettier.config')
 
 /** @type {import("prettier").Config} */
 const config = {
   ...baseConfig,
-  plugins: [...(baseConfig.plugins ?? []), tailwind],
+  plugins: [...(baseConfig.plugins ?? []), 'prettier-plugin-tailwindcss'],
 }
 
 module.exports = config
